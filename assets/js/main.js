5ed203d646380351403012b04abb8d636e9240dd
sabinafloreano <sabina.floreano@gmail.com>
2016-07-12 16:43:34 +0800

    Def

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,9 +39,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'images/urban.jpg': 'center',
+							'images/street.jpg': 'center',
+							'images/party.jpg': 'center'
 						},
 
 					// Delay.
